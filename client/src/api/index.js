d2e90a2f32608e57807dadcad1ad4360a093449e
waynehuang <waynehuang119@hotmail.com>
2024-09-27 14:17:15 -0500

    update base url and _redirects in client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://photo-kanban-server-672kjrumj-waynehuang-119s-projects.vercel.app/posts';
+const url = 'https://photo-kanban-server-a4mcctjiv-waynehuang-119s-projects.vercel.app/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
