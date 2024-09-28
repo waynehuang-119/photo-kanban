ca3911bdb0d33ed4dd15349541e90c6d761a45b0
waynehuang <waynehuang119@hotmail.com>
2024-09-28 11:00:04 -0500

    updated api url at client side

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = '/posts';
+const url = 'https://photo-kanban-server.vercel.app/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
